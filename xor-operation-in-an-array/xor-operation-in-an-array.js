56815f840f93e0fb6dd18ede0339a27d4df84e85
Fikri Anuar <70595834+FikriAnuar@users.noreply.github.com>
2021-10-29 02:33:10 -0400

    Time: 68 ms (90.27%), Space: 38.8 MB (26.49%) - LeetHub

---
+++ xor-operation-in-an-array/xor-operation-in-an-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+  let result = 0;
+  for (var i = 0; i < n; i++) {
+    result = result ^ (start + 2*i);
+  }
+  return result;
+};
